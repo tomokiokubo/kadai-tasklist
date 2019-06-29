c724952d41d286982febe5e2e5300223d97a0b8f
tomoki.okubo <tomoki_1007@docomo.ne.jp>
2019-06-29 06:02:01 +0000

    secondgit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,8 +48,13 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
 
   def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content,:status)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_045632) do
+ActiveRecord::Schema.define(version: 2019_06_27_102324) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
